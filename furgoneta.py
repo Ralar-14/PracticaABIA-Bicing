b84f90e08efd8162da8c71b654f89f56f60ae3f5
Pololo04 <pol.perez.prades@estudiantat.upc.edu>
2023-10-18 13:14:11 +0200

    #6
    generate furgonetas simple y heap greedy.

--- furgoneta.py
+++ furgoneta.py
@@ -1,10 +1,10 @@
 import abia_bicing
 from abia_bicing import Estaciones, Estacion
 from Bicing_problem_parameters import ProblemParameters
-
+import heapq
 # 
 class Furgoneta(object):
-    def __init__(self, id: int, origen: tuple, ToGo: list = [(-1, -1), (-1, -1)], carga: tuple = [0, 0]) -> None:
+    def __init__(self, id: int, origen: Estacion, ToGo: list = [None, None], carga: list = [0, 0]) -> None:
         self.max_bicis = 30
         self.id = id
         self.origen = origen
@@ -25,19 +25,43 @@ class Furgonetas(object):
         self.num_furgonetas = num_furgonetas
         self.estaciones = estaciones
         self.lista_furgonetas = []
-        self.__genera_furgonetas()
+        self.__genera_furgonetes_senzill()
         
     def __genera_furgonetas(self):
         # Ordenamos las estaciones segun el numero de bicicletas sobrantes que tienen
         self.estaciones.lista_estaciones.sort(key=lambda x: x.num_bicicletas_next - x.demanda, reverse=True)
         
-        for i in range(self.num_furgonetas):
-            bicis_sobrantes = self.estaciones.lista_estaciones[i].num_bicicletas_next - self.estaciones.lista_estaciones[i].demanda
-            self.lista_furgonetas.append(Furgoneta(i, self.estaciones.lista_estaciones[i].get_origen(), carga = [bicis_sobrantes if bicis_sobrantes <= 30 else 30, 0]))
-        
-        self.estaciones.lista_estaciones.sort(key=lambda x: x.demanda, reverse=True)
+        heap = []
+        heap2 = []
+        for est in self.estaciones.lista_estaciones:
+            bicis_sobrantes = est.num_bicicletas_next - est.demanda
+            heapq.heappush(heap2, (bicis_sobrantes,est))
+            heapq.heappush(heap, (-bicis_sobrantes,est))
 
-        for i in range(self.estaciones):
+        for i in range(self.num_furgonetas):
+            bicis_sobrantes = -heap[0][0]
+            carga = [bicis_sobrantes if bicis_sobrantes <= 30 else 30, 0]
+            self.lista_furgonetas.append(Furgoneta(i, self.heap[0][1],  carga))
+            heap[0][0] = -(bicis_sobrantes - carga[0])
+            heapq.heapify(heap)
+        
+        for furgo in self.lista_furgonetas:
+            furgo.ToGo[0] = heap2[0][1]
+            heapq.heapify(heap2)
+
+        for furgo in self.lista_furgonetas:
+            furgo.ToGo[1] = heap2[0][1]
+#################
+# No està acabat
+# ##############################################
+
+    def __genera_furgonetes_senzill(self):
+        i = 0
+        a = 0
+        while i < self.num_furgonetas:
+            self.lista_furgonetas.append(Furgoneta(i, self.estaciones.lista_estaciones[a], [self.estaciones.lista_estaciones[a+1], self.estaciones.lista_estaciones[a+2]]))
+            a += 3
+            i += 1
 
     def __repr__(self):
         return f"Furgonetas({self.lista_furgonetas}, \n \n{self.estaciones})"
